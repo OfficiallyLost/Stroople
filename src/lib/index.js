7e633ec961ecabe0fe805d9be7cd64d56f6b99aa
haseeb <farooqhaseeb49@gmail.com>
2020-07-08 04:17:47 +0100

    add calc function

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,26 +1,80 @@
 'use-strict';
 
 class Mathematics {
+	/**
+    * 
+    * @param {Number} number a number to round to the nearest whole number
+    */
 	round = (number) => {
 		if (typeof number !== 'number')
 			throw TypeError(`Typeof number must be a type of 'number', not '${typeof number}'`);
 		else return Math.round(number);
 	};
-
+	/**
+    * 
+    * @param  {...Number} number a list of numbers seperates by ',' to add
+    */
 	add = (...number) => {
-		return number.reduce((a, b) => a + b);
+		if (typeof number !== 'number')
+			throw TypeError(`Typeof number must be a type of 'number', not '${typeof number}'`);
+		else return number.reduce((a, b) => a + b);
 	};
-
+	/**
+    * 
+    * @param  {...Number} number a list of numbers seperates by ',' to subtract
+    */
 	subtract = (...number) => {
-		return number.reduce((a, b) => a - b);
+		if (typeof number !== 'number')
+			throw TypeError(`Typeof number must be a type of 'number', not '${typeof number}'`);
+		else return number.reduce((a, b) => a - b);
 	};
-
+	/**
+    * 
+    * @param  {...Number} number a list of numbers seperates by ',' to divide
+    */
 	divide = (...number) => {
-		return number.reduce((a, b) => a / b);
+		if (typeof number !== 'number')
+			throw TypeError(`Typeof number must be a type of 'number', not '${typeof number}'`);
+		else return number.reduce((a, b) => a / b);
 	};
-
+	/**
+    * 
+    * @param  {...Number} number a list of numbers seperates by ',' to multiply
+    */
 	multiply = (...number) => {
-		return number.reduce((a, b) => a * b);
+		if (typeof number !== 'number')
+			throw TypeError(`Typeof number must be a type of 'number', not '${typeof number}'`);
+		else return number.reduce((a, b) => a * b);
+	};
+	/**
+    * 
+    * @param {String} string a random string to calculate
+    */
+	calc = (string) => {
+		switch (true) {
+			case string.includes('*'):
+				return string.split('*').reduce((a, b) => a * b);
+			case string.includes('-'):
+				return string.split('-').reduce((a, b) => a - b);
+			case string.includes('+'):
+				return string.split('+').reduce((a, b) => a + b);
+			case string.includes('/'):
+				return string.split('/').reduce((a, b) => a / b);
+			case string.toLowerCase() === 'pi':
+				return Math.PI;
+			case string.toLowerCase() === 'e':
+				return Math.E;
+			case string.toLowerCase() === 'LN10':
+				return Math.LN10;
+			case string.toLowerCase() === 'LN2':
+				return Math.LN2;
+			case string.toLowerCase() === 'LOG10E':
+				return Math.LOG10E;
+			case string.toLowerCase() === 'SQRT1_2':
+				return Math.SQRT1_2;
+			case string.toLowerCase() === 'SQRT2':
+				return Math.SQRT2;
+		}
 	};
 }
 
