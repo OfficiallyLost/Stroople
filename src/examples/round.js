c143c1a94c636ef8b88084b77d3fc33c1c2658cc
haseeb <farooqhaseeb49@gmail.com>
2020-07-13 02:30:50 +0100

    update round example

--- src/examples/round.js
+++ src/examples/round.js
@@ -5,3 +5,12 @@ const math = new Stroople();
 
 math.round(1.5); // Expected output: 2
 math.round(1.2); // Expected output: 1
+
+// Want to round numbers to a different Decimal Place?
+// NOTE: THIS IS OPTIONAL AS THE DEFAULT IS TO THE WHOLE NUMBER
+
+const Stroople = require('strooplejs');
+const math = new Stroople();
+
+math.round(1.5222, 1); // Expected output: 1.5
+math.round(333.3333, 2); // Expected output: 333.33
